a99aa0d624f71d2016a6a16463307d77a446c1f1
KaPToHka <57670787+Sabirzyanov@users.noreply.github.com>
2022-03-21 21:27:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Foo f = new Foo();
+        f.OnKeyPressed += (_, c) => Console.WriteLine(c);
+        f.Run();
+    }
+}
+
+public class Foo
+{
+    public event EventHandler<char> OnKeyPressed;
+
+    public void Run()
+    {
+        char input = Console.ReadKey().KeyChar;
+        while (input != 'c')
+        {
+            OnKeyPressed?.Invoke(this, input);
+            input = Console.ReadKey().KeyChar;
+        }
+    }
+}
